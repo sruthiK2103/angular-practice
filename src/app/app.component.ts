efdf134a94b78fcd62640bc3ddc15d3250fe55e6
sruthi <sruthi.kovvuru@engageveracity.com>
2018-05-16 07:54:17 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name='all';
+  userName = '';
+
+  emptyUserName() {
+    this.userName = '';
+  }
 }
