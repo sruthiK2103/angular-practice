5904fd6b2a43b9158071d6bf33cde6b7ee9b675a
sruthiK2103 <sruthik.2103@gmail.com>
2018-05-16 12:04:22 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name='all';
   userName = '';
+  display = false;
+  btnClicks = [];
+  
 
   emptyUserName() {
     this.userName = '';
+  }
+  displayDetails() {
+    
+    this.display = !this.display;
+    //this.btnClicks.push(this.btnClicks.length + 1);
+    this.btnClicks.push(new Date());
   }
 }
